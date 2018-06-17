aad0202614f0b607d62dbfccde57ceb4e98e314c
Srilalitha <Srilalitha@Srilalitha-VAIO.mshome.net>
2018-06-17 11:48:27 +0530

    3rd commit. Created frontend with folders controllers,services,views,resources. created usercontroller.js, userservice.js and view pages containing index,home,login,registrationform.html pages.

---
+++ SHAREUPfrontend/WebContent/services/userservice.js
@@ -0,0 +1,26 @@
+/**
+ * UserService
+ * 1. Register - call the middleware -> dao -> insert user details in User table
+ */
+app.factory('UserService',function($http){
+	var userService={}
+	
+	userService.register=function(user){
+		return $http.post("http://localhost:8097/SHAREUPmiddleware/register",user)
+	}
+	
+	userService.login=function(user){
+		return $http.post("http://localhost:8097/SHAREUPmiddleware/login",user)
+	}
+	
+	userService.logout=function(){
+		return $http.put("http://localhost:8097/SHAREUPmiddleware/logout")
+	}
+     
+	userService.updateProfile=function(user){//updated user profile
+		return $http.put("http://localhost:8097/SHAREUPmiddleware/update",user)
+	}
+	
+	return userService;
+})
+
