04ae63da4cdb7f494ed017d2bb1bf48610d3c512
Srilalitha <Srilalitha@Srilalitha-VAIO.mshome.net>
2018-06-30 20:13:19 +0530

    16th commit. Created entity profile picture and its table in backend.

--- SHAREUPbackend/src/main/java/com/niit/configuration/DBConfiguration.java
+++ SHAREUPbackend/src/main/java/com/niit/configuration/DBConfiguration.java
@@ -16,6 +16,7 @@ import com.niit.model.BlogComment;
 import com.niit.model.BlogPost;
 import com.niit.model.BlogPostLikes;
 import com.niit.model.Job;
+import com.niit.model.ProfilePicture;
 import com.niit.model.User;
 
 
@@ -40,7 +41,7 @@ public class DBConfiguration {
 		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
 		hibernateProperties.setProperty("hibernate.show_sql", "true");
 		lsf.addProperties(hibernateProperties);
-		Class classes[]=new Class[]{User.class,Job.class,BlogPost.class,BlogPostLikes.class,BlogComment.class};//class objects of all entities
+		Class classes[]=new Class[]{User.class,Job.class,BlogPost.class,BlogPostLikes.class,BlogComment.class,ProfilePicture.class};//class objects of all entities
 	    return lsf.addAnnotatedClasses(classes).buildSessionFactory();
 	}
 	@Bean
